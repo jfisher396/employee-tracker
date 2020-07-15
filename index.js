6f1217232d8adfee186200da8395fde5a1adae4d
James Fisher <jfisher396@hotmail.com>
2020-07-14 22:51:26 -0700

    working on add function

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ function initialQuery() {
                     break;
 
                 case "Add department, roles or employees":
-                    multiSearch();
+                    addValue();
                     break;
 
                 case "Update employee role":
@@ -59,8 +59,8 @@ function initialQuery() {
                     break;
 
                 case "Exit":
-                connection.end();
-                break;
+                    connection.end();
+                    break;
             }
         });
 }
@@ -94,120 +94,30 @@ function viewTable() {
     })
 }
 
-
-// function multiSearch() {
-//     var query = "SELECT artist FROM top5000 GROUP BY artist HAVING count(*) > 1";
-//     connection.query(query, function (err, res) {
-//         for (var i = 0; i < res.length; i++) {
-//             console.log(res[i].artist);
-//         }
-//         runSearch();
-//     });
-// }
-
-// function rangeSearch() {
-//     inquirer
-//         .prompt([{
-//                 name: "start",
-//                 type: "input",
-//                 message: "Enter starting position: ",
-//                 validate: function (value) {
-//                     if (isNaN(value) === false) {
-//                         return true;
-//                     }
-//                     return false;
-//                 }
-//             },
-//             {
-//                 name: "end",
-//                 type: "input",
-//                 message: "Enter ending position: ",
-//                 validate: function (value) {
-//                     if (isNaN(value) === false) {
-//                         return true;
-//                     }
-//                     return false;
-//                 }
-//             }
-//         ])
-//         .then(function (answer) {
-//             var query = "SELECT position,song,artist,year FROM top5000 WHERE position BETWEEN ? AND ?";
-//             connection.query(query, [answer.start, answer.end], function (err, res) {
-//                 for (var i = 0; i < res.length; i++) {
-//                     console.log(
-//                         "Position: " +
-//                         res[i].position +
-//                         " || Song: " +
-//                         res[i].song +
-//                         " || Artist: " +
-//                         res[i].artist +
-//                         " || Year: " +
-//                         res[i].year
-//                     );
-//                 }
-//                 runSearch();
-//             });
-//         });
-// }
-
-// function songSearch() {
-//     inquirer
-//         .prompt({
-//             name: "song",
-//             type: "input",
-//             message: "What song would you like to look for?"
-//         })
-//         .then(function (answer) {
-//             console.log(answer.song);
-//             connection.query("SELECT * FROM top5000 WHERE ?", {
-//                 song: answer.song
-//             }, function (err, res) {
-//                 console.log(
-//                     "Position: " +
-//                     res[0].position +
-//                     " || Song: " +
-//                     res[0].song +
-//                     " || Artist: " +
-//                     res[0].artist +
-//                     " || Year: " +
-//                     res[0].year
-//                 );
-//                 runSearch();
-//             });
-//         });
-// }
-
-// function songAndAlbumSearch() {
-//     inquirer
-//         .prompt({
-//             name: "artist",
-//             type: "input",
-//             message: "What artist would you like to search for?"
-//         })
-//         .then(function (answer) {
-//             var query = "SELECT top_albums.year, top_albums.album, top_albums.position, top5000.song, top5000.artist ";
-//             query += "FROM top_albums INNER JOIN top5000 ON (top_albums.artist = top5000.artist AND top_albums.year ";
-//             query += "= top5000.year) WHERE (top_albums.artist = ? AND top5000.artist = ?) ORDER BY top_albums.year, top_albums.position";
-
-//             connection.query(query, [answer.artist, answer.artist], function (err, res) {
-//                 console.log(res.length + " matches found!");
-//                 for (var i = 0; i < res.length; i++) {
-//                     console.log(
-//                         i + 1 + ".) " +
-//                         "Year: " +
-//                         res[i].year +
-//                         " Album Position: " +
-//                         res[i].position +
-//                         " || Artist: " +
-//                         res[i].artist +
-//                         " || Song: " +
-//                         res[i].song +
-//                         " || Album: " +
-//                         res[i].album
-//                     );
-//                 }
-
-//                 runSearch();
-//             });
-//         });
-// }
+function addValue() {
+    inquirer.prompt({
+        name: "add",
+        type: "list",
+        message: "Which would you like to add?",
+        choices: ["Department", "Role", "Employee"]
+    }).then(val => {
+        if (val.add === "Department") {
+            inquirer.prompt({
+                type: "input",
+                name: "dept_add",
+                message: "What is the name of the department you would like to add?"
+            }).then(function(answer) {
+                console.log(answer.dept_add);
+                connection.query("INSERT INTO Departments SET ?", 
+                    {
+                        name: answer[],
+                    },
+                    function (err, res) {
+                        if (err) throw err;
+                        initialQuery();
+                    }
+                )
+            });
+        }
+    })
+}
