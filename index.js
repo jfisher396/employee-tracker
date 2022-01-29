b6ec72dee6e96bc65b3b17b8531a83140059570a
James Fisher <jfisher396@hotmail.com>
2022-01-29 14:40:57 -0600

    updated var to const in index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
+const mysql = require("mysql");
+const inquirer = require("inquirer");
 
-var connection = mysql.createConnection({
+
+const connection = mysql.createConnection({
   host: "localhost",
   // Your port; if not 3306
   port: 3306,
@@ -200,6 +201,7 @@ function addValue() {
 }
 
 function updateRole() {
+    console.table()
   inquirer
     .prompt([
       {
